02d3f8fabec2ea10b84d518f16d041eeb7ed2487
runope <275358499@qq.com>
2021-06-01 08:40:22 +0800

    modification tscompile.py

--- tscompile.py
+++ tscompile.py
@@ -2,7 +2,7 @@
 Author: Runope
 Date: 2021-05-31 16:50:17
 LastEditors: Runope
-LastEditTime: 2021-05-31 16:58:05
+LastEditTime: 2021-06-01 08:38:33
 Description: run script
 contact: runope@qq.com
 '''
@@ -14,5 +14,5 @@ import sys
 srcPath = sys.argv[1]
 
 if __name__ == "__main__":
-    os.system('yarn frida-compile src/' + srcPath + '.ts -o dist/_' + srcPath + '.js -w')
+    os.system('yarn frida-compile ' + srcPath + ' -o dist/_agent.js -w')
     pass
