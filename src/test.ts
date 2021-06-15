485da67a3833dbaf9f016c0d678e2401d1c0cba6
runope <275358499@qq.com>
2021-06-15 19:01:01 +0800

    modify

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@
  * @Date: 2021-05-31 17:00:04
  *
  * @LastEditors: Runope
- * @LastEditTime: 2021-06-04 17:21:59
+ * @LastEditTime: 2021-06-15 14:11:27
  * @Description: file content
  * @contact: runope@qq.com
  */
@@ -12,11 +12,28 @@ import { unpack } from "../agent/utils/android/unpack";
 import { print } from "../agent/logger";
 import { hooklib } from "../agent/utils/android/hooklib";
 import { anti } from "../agent/utils/android/anti";
+import {hook_decode} from "../agent/utils/android/decodehook"
 
 function exploit() {
     Java.perform(() => {
 
         anti.anti_debug()
+        hook_decode()
+
+
+        // const shield_base:any = Module.findBaseAddress("libshield.so")
+
+        // const sub_ABB8 = ptr(shield_base).add(0xabb8).add(0x1)
+
+        // Interceptor.attach(sub_ABB8, {
+        //     onEnter: (args) => {
+        //         print(hexdump(args[0], {length: 256}))
+        //     },
+        //     onLeave: (retval) => {
+
+        //     }
+        // })
+
     });
 }
 
